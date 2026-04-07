a9f117ba6118e75a5e878a0e97e2f2f41e39c825
m.crews <michael.crews@salesagility.com>
2026-04-07 22:50:54 +0100

    frontend changes

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
 <?php
 
-use App\models\UserModel;
-use App\models\VideoGameModel;
-
 require './app/bootstrap.php';
-
-$user = new UserModel('admin', 'admin');
-
-echo $user->getUsername();
-
-$videoGame = new VideoGameModel();
-echo 'Test2';
+$pageContents = file_get_contents('./public/index_page.html');
+echo $pageContents;
